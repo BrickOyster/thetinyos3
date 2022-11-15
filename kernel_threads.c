6b12011d9246199ed21cedf1c84210d1c5b88f07
AkisKel <akiskelaidis@gmail.com>
2022-11-15 19:57:09 +0200

    ThreadExit_too

--- kernel_threads.c
+++ kernel_threads.c
@@ -56,7 +56,7 @@ Tid_t sys_CreateThread(Task task, int argl, void* args)
  */
 Tid_t sys_ThreadSelf()
 {
-	return (Tid_t) cur_thread();
+	return (Tid_t) (cur_thread()->ptcb);
 }
 
 /**
@@ -80,6 +80,78 @@ int sys_ThreadDetach(Tid_t tid)
   */
 void sys_ThreadExit(int exitval)
 {
+  TCB* cur_tcb = cur_thread();
+  PTCB* cur_ptcb =  cur_tcb->ptcb;
+  cur_ptcb->exitval = exitval;
 
+  cur_ptcb->exited = 1;
+  cur_ptcb->refcount--;
+
+  PCB* curproc = CURPROC;
+  curproc->thread_count--;
+
+  // wake up all the threads waiting on this one 
+  kernel_broadcast(&cur_ptcb->exit_cv);
+
+  /* if this is the last thread of the current process*/
+  if(curproc->thread_count==0){
+    if(get_pid(curproc)!=1){
+      /* Reparent any children of the exiting process to the 
+      initial task */
+      PCB* initpcb = get_pcb(1);
+      while(!is_rlist_empty(& curproc->children_list)) {
+        rlnode* child = rlist_pop_front(& curproc->children_list);
+        child->pcb->parent = initpcb;
+        rlist_push_front(& initpcb->children_list, child);
+      }
+
+      /* Add exited children to the initial task's exited list 
+         and signal the initial task */
+      if(!is_rlist_empty(& curproc->exited_list)) {
+        rlist_append(& initpcb->exited_list, &curproc->exited_list);
+        kernel_broadcast(& initpcb->child_exit);
+      }
+
+      /* Put me into my parent's exited list */
+      rlist_push_front(& curproc->parent->exited_list, &curproc->exited_node);
+      kernel_broadcast(& curproc->parent->child_exit);
+    }
+    
+    assert(is_rlist_empty(& curproc->children_list));
+    assert(is_rlist_empty(& curproc->exited_list));
+
+
+    /* 
+      Do all the other cleanup we want here, close files etc. 
+    */
+    
+    /* Release the args data */
+    if(curproc->args) {
+      free(curproc->args);
+      curproc->args = NULL;
+    }
+
+    /* Clean up FIDT */
+    for(int i=0;i<MAX_FILEID;i++) {
+      if(curproc->FIDT[i] != NULL) {
+        FCB_decref(curproc->FIDT[i]);
+        curproc->FIDT[i] = NULL;
+      }
+    }
+
+    if(cur_ptcb->refcount == 0){
+      rlist_remove(&cur_ptcb->ptcb_list_node);  // remove the PTCB from the PCB's list
+      free(cur_ptcb);                          // free the PTCB
+    }
+
+    /* Disconnect my main_thread */
+    curproc->main_thread = NULL;
+
+    /* Now, mark the process as exited. */
+    curproc->pstate = ZOMBIE;
+  }
+
+  /* Bye-bye cruel world */
+  kernel_sleep(EXITED, SCHED_USER);
 }
 
